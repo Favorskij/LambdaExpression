45776dc618c255cd57b972477f50d8508f442291
favorskij <favorskij@mail.ru>
2020-10-27 20:42:18 +1100

    Commit

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Lambda expression
 
 **Базовый пример использования Lambda expression**
+
+- https://www.youtube.com/watch?v=Wc4CIUlfpWg
+- https://www.youtube.com/watch?v=glVHgOtHtSk
+- https://www.youtube.com/watch?v=SS2wP0ALxZE
+- https://www.youtube.com/watch?v=jkqo8Nwqouc
--- src/com/lambdaexpression/Main.java
+++ src/com/lambdaexpression/Main.java
@@ -1,35 +1,51 @@
 package com.lambdaexpression;
 
+// 1 - https://www.youtube.com/watch?v=Wc4CIUlfpWg
+// 2 - https://www.youtube.com/watch?v=glVHgOtHtSk
+// 3 - https://www.youtube.com/watch?v=SS2wP0ALxZE
+// 4 - https://www.youtube.com/watch?v=jkqo8Nwqouc
+
 public class Main {
 
     public static void main(String[] args) {
 
         SumImpl sum1 = Integer::sum;
-        System.out.println(sum1.sum(1,2));
+        System.out.println("1 == " + sum1.sum(1,2));
 
         SumImpl sum2 = (a, b) -> a + b;
-        System.out.println(sum2.sum(2,3));
+        System.out.println("2 == " + sum2.sum(1,2));
+
+        ///////////////////////////////////////
 
         SumImpl sum3 = (a, b) -> {
             return a + b;
         };
-        System.out.println(sum3.sum(3,4));
+        System.out.println("3 == " + sum3.sum(3,4));
+
+        ///////////////////////////////////////
 
         Sqrt sqrt1 = x -> x + 2;
-        System.out.println(sqrt1.sqrt(5));
+        System.out.println("4 == " + sqrt1.sqrt(5));
+
+        ///////////////////////////////////////
 
         Sqrt sqrt2 = x -> Math.sqrt(x);
-        System.out.println(sqrt2.sqrt(6));
-
+        System.out.println("5 == " + sqrt2.sqrt(6));
 
         Sqrt sqrt3 = Math::sqrt;
-        System.out.println(sqrt3.sqrt(7));
+        System.out.println("6 == " + sqrt3.sqrt(6));
+
+        ///////////////////////////////////////
 
         Printer printer1 = x -> System.out.println(x);
         printer1.sqrt(8);
 
         Printer printer2 = System.out::println;
         printer2.sqrt(8);
+
+
+
+
     }
 
 
